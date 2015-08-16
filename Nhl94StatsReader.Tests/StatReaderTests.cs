5b90a08764b9f4b100d3be0f5d70684207fe4b32
Mhopkinsinc <mhopkinsinc@gmail.com>
2015-08-15 20:29:42 -0400

    Added Method To Check The Save State header. Added Unit Test

---
+++ Nhl94StatsReader.Tests/StatReaderTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Nhl94StatsReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Nhl94StatsReader.Tests
+{
+    public class StatReaderTests
+    {
+        [Fact()]
+        public void ValidateZsnesSaveStateTest()
+        {
+
+            //Set The Path Of The ZSNES Save State File            
+            var _saveStatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"SaveState\nhl94.zs3");
+                        
+            var sr = new StatReader(_saveStatePath);            
+
+            Assert.True(sr.ValidSaveStateFile, "Valid ZSNES File");
+        }
+    }
+}
